e8b6e113754e693708a682a70fa19e3c18b7c2a9
nopalsisfo2025 <nopalk6372@gmail.com>
2025-11-10 01:01:15 +0800

    Add files via upload

---
+++ LUAS PERSEGI.py
@@ -0,0 +1,11 @@
+# nama : Nopal
+# nim : D0425005
+# luas persegi
+
+
+s = int(input("masukkan sisi pertama: "))
+s2 = int(input("Masukkan sisi kedua: "))
+
+luas = int(s * s2 ) 
+
+print('luas persegi adalah: ' , luas)

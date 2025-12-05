4280dee1d1c464d17c65d101a824fd2f7672292e
nopalsisfo2025 <nopalk6372@gmail.com>
2025-12-05 16:41:19 +0800

    Add files via upload

---
+++ tgs menghitung luas/LUAS SEGITIGA.py
@@ -0,0 +1,11 @@
+# nama : Nopal
+# nim : D0425005
+# luas segitiga
+
+
+L = int(input("masukkan Alas segitiga: "))
+T = int(input("Masukkan tinggi segitiga: "))
+
+luas = int((L * T ) / 2)
+
+print('luas segitiga adalah: ' , luas)
